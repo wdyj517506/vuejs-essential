ef86494ff596ddecc5405acee58370ea427f4c62
wdyj517506 <13760610560@163.com>
2018-11-20 14:25:42 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<h1>Welcome to Marke web</h1>
+	</div>
+</template>
+<script>
+	export default{
+		name:'App'
+	}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+}
+</style>
